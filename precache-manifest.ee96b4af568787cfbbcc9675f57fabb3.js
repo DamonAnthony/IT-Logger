40067ec0b204a6686ab19790fe10b16545b74fab
Damon Anthony <damonanthony125@gmail.com>
2020-07-07 13:30:40 +0200

    Updates

---
+++ precache-manifest.ee96b4af568787cfbbcc9675f57fabb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654c29fba919a6d2c7997448d4920be0",
+    "url": "/IT-Logger/index.html"
+  },
+  {
+    "revision": "bbcae626be9621107e42",
+    "url": "/IT-Logger/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "24fba77362318d24dfb7",
+    "url": "/IT-Logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "bbcae626be9621107e42",
+    "url": "/IT-Logger/static/js/2.2e1f5de1.chunk.js"
+  },
+  {
+    "revision": "1c20bd7d321bb86647478cb91219cbe0",
+    "url": "/IT-Logger/static/js/2.2e1f5de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fba77362318d24dfb7",
+    "url": "/IT-Logger/static/js/main.c63ee67f.chunk.js"
+  },
+  {
+    "revision": "b077849cd1d2f33a1518",
+    "url": "/IT-Logger/static/js/runtime-main.21df4f96.js"
+  }
+]);
